4c215084ee9837a81166919a6d46ba7ff46ad2b0
Anton Miranovich <antonmir992@gmail.com>
2023-08-05 17:51:57 +0300

    gtn work4

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-function benar() {
-  const numRandom = Math.floor(Math.random * 100);
+const numRandom = Math.floor(Math.random() * 100);
+
+function Binar(numRandomFun) {
   let start = 0;
   let end = 100;
   let count = 0;
-  for (let i = start; i < end; i++) {
-    let value = prompt(
-      "Программа задает рандомное значение. Ваша задача угадать его. Введите число от 0 до 100"
+  for (let i = 0; i < 100; i++) {
+    let value = +prompt(
+      `Программа задает рандомное значение. Ваша задача угадать его. Введите число от ${start} до ${end}`
     );
     count++;
-    if (value == numRandom) {
-      alert(`Вы угадали. Поздровляем!!! Вам понадобилось попыток ${count}`);
-      break
+    if (value == numRandomFun) {
+     alert(`Вы угадали. Поздровляем!!! Вам понадобилось попыток ${count}`);
+      break;
+    } else if (value > numRandomFun) {
+      end = value;
+      alert(
+        `Вы ввели число ${value} и это слишком большое значение. Введите число от ${start} до ${end}`
+      );
+    } else if (value < numRandomFun) {
+      start = value;
+      alert(
+        `Вы ввели число ${value} и это слишком маленькое значение. Введите число от ${start} до ${end}`
+      )
     }
   }
 }
 
-benar();
+Binar(numRandom);
+
